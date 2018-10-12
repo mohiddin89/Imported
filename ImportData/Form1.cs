1cadf5fbb9ba3a1d26af1d57204c02a095d607e4
vaibhav <vaibhav@DESKTOP-CUB067>
2018-10-12 19:59:07 +0530

    About tool

--- ImportData/Form1.cs
+++ ImportData/Form1.cs
@@ -22,7 +22,7 @@ namespace ImportData
 
         private void Form1_Load(object sender, EventArgs e)
         {
-
+             
         }
 
         private void button1_Click(object sender, EventArgs e)
